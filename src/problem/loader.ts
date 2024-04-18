6366202f89bb3ce9ba569d34e854fa632a983957
cn_ryh <15543677192@163.com>
2024-04-18 17:06:51 +0800

    测试路由

--- src/problem/loader.ts
+++ src/problem/loader.ts
@@ -15,8 +15,10 @@ const routes = [
 // 3. 创建路由实例并传递 `routes` 配置
 // 你可以在这里输入更多的配置，但我们在这里
 // 暂时保持简单
+
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory(``),
+    history: VueRouter.createWebHashHistory(`problem`),
     routes,
+    
 });
 export { router };
